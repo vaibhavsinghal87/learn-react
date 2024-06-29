c83eb9938823fd522f0ed135a042f6e3aad23871
Vaibhav Singhal <vaibhavsinghal87@gmail.com>
2024-06-29 14:09:35 +0000

    react basics

---
+++ basics/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import reactImg from '../assets/react-core-concepts.png';
+
+const desc = ["Core", "Fundamental", "Critical"];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+    const value = desc[getRandomInt(2)];
+    return (
+        <header>
+            <img src={reactImg} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {value} React concepts you will need for almost any app you are
+                going to build!
+            </p>
+        </header>
+    );
+}
