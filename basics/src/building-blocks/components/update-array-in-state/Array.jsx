06b44908df41215ecf387984695e892cf3fde907
VS <vaibhavsinghal87@gmail.com>
2026-03-08 23:31:16 +0530

    update array in state

--- basics/src/building-blocks/components/update-array-in-state/Array.jsx
+++ basics/src/building-blocks/components/update-array-in-state/Array.jsx
@@ -2,24 +2,48 @@ import { useState } from "react";
 
 let id = 0;
 export default function Array() {
-  const [name, setName] = useState("");
+  const [index, setIndex] = useState(0);
+  const [name, setName] = useState("value");
   const [people, setPeople] = useState([]);
   const handleChange = (e) => {
     setName(e.target.value);
   };
   const handleClick = () => {
     setPeople([...people, { id: id++, name }]);
+    // setPeople([{ id: id++, name }, ...people]);
     setName("");
+  };
+  const handleDelete = (id) => () => {
+    setPeople(people.filter((p) => p.id !== id));
+  };
+  const handleIndexChange = (e) => {
+    setIndex(e.target.value);
+  };
+  const handleIndexClick = () => {
+    const newPeople = [...people];
+    newPeople.splice(index, 0, { id: id++, name });
+    setPeople(newPeople);
+    setName("");
+  };
+  const handleReverse = () => {
+    setPeople([...people].reverse());
   };
   return (
     <>
       <div>
         <input value={name} onChange={handleChange} />
+        <input value={index} onChange={handleIndexChange} />
+        <br />
         <button onClick={handleClick}>Add</button>
+        <button onClick={handleIndexClick}>Add at index</button>
+        <button onClick={handleReverse}>Reverse</button>
       </div>
       <ul>
         {people.map((p) => (
-          <li key={p.id}>{p.name}</li>
+          <li key={p.id}>
+            {p.name}
+            <button onClick={handleDelete(p.id)}>Delete</button>
+          </li>
         ))}
       </ul>
     </>
