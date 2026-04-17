b19274833fc70917ea9378fdcf26d85a51e2b51b
VS <vaibhavsinghal87@gmail.com>
2026-04-18 01:49:33 +0530

    pnpm react monorepo setup

---
+++ apps/monorepo-mfes/pnpm/apps/platform/src/App.tsx
@@ -0,0 +1,15 @@
+import { UIButton } from "@foundation/core-lib";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>Platform app</div>
+        <UIButton></UIButton>
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ apps/monorepo-mfes/pnpm/packages/core-lib/src/components/button/Button.tsx
@@ -0,0 +1,5 @@
+import Button from "@mui/material/Button";
+
+export default function UIButton() {
+  return <Button variant="contained">Click Me!!!</Button>;
+}
